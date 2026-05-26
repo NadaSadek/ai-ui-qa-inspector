644b2bffa4b1084c6b580a27295992827e493950
Nada Sadek <9610904+NadaSadek@users.noreply.github.com>
2026-05-26 11:43:12 +0200

    Simplify inspection request body by directly using inspectionTarget

--- src/components/qa-inspector/QAInspectorView.tsx
+++ src/components/qa-inspector/QAInspectorView.tsx
@@ -33,14 +33,7 @@ export function QAInspectorView() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          id: inspectionTarget.id,
-          screenName: inspectionTarget.screenName,
-          stateName: inspectionTarget.stateName,
-          description: inspectionTarget.description,
-          screenshotDescription: inspectionTarget.screenshotDescription,
-          domSnippet: inspectionTarget.domSnippet,
-        }),
+        body: JSON.stringify(inspectionTarget),
       });
 
       if (!response.ok) {
